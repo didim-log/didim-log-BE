531b6ad298cb17478eaed3c68758c67ed17d2e34
stdiodh <playlistdh@gmail.com>
2025-11-25 14:45:45 +0900

    chore: project init

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.8"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.didimlog"
+version = "0.0.1-SNAPSHOT"
+description = "Step by step algorithm log"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/didimlog/DidimLogApplication.kt
@@ -0,0 +1,11 @@
+package com.didimlog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DidimLogApplication
+
+fun main(args: Array<String>) {
+	runApplication<DidimLogApplication>(*args)
+}
