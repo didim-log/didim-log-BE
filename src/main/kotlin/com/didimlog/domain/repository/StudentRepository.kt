b042a4dcbf3760821a35be323201dcb6ccc40010
stdiodh <playlistdh@gmail.com>
2025-12-04 16:11:50 +0900

    feat(user): 사용자 프로필 수정(닉네임, 비번) 비즈니스 로직 구현

--- src/main/kotlin/com/didimlog/domain/repository/StudentRepository.kt
+++ src/main/kotlin/com/didimlog/domain/repository/StudentRepository.kt
@@ -2,6 +2,7 @@ package com.didimlog.domain.repository
 
 import com.didimlog.domain.Student
 import com.didimlog.domain.valueobject.BojId
+import com.didimlog.domain.valueobject.Nickname
 import java.util.Optional
 import org.springframework.data.mongodb.repository.MongoRepository
 
@@ -10,6 +11,8 @@ interface StudentRepository : MongoRepository<Student, String> {
     fun findByBojId(bojId: BojId): Optional<Student>
 
     fun existsByBojId(bojId: BojId): Boolean
+
+    fun existsByNickname(nickname: Nickname): Boolean
 }
 
 
