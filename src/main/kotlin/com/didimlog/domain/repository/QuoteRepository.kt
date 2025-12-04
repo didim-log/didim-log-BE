f80585a284d891485955cb77aa46ef29ee4dcb68
stdiodh <playlistdh@gmail.com>
2025-12-04 16:14:28 +0900

    feat(quote): 명언 서비스 구현 및 초기 데이터 시딩 로직 추가

---
+++ src/main/kotlin/com/didimlog/domain/repository/QuoteRepository.kt
@@ -0,0 +1,8 @@
+package com.didimlog.domain.repository
+
+import com.didimlog.domain.Quote
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface QuoteRepository : MongoRepository<Quote, String> {
+}
+
---
+++ src/main/kotlin/com/didimlog/ui/dto/QuoteResponse.kt
@@ -0,0 +1,23 @@
+package com.didimlog.ui.dto
+
+import com.didimlog.domain.Quote
+
+/**
+ * 명언 응답 DTO
+ */
+data class QuoteResponse(
+    val id: String,
+    val content: String,
+    val author: String
+) {
+    companion object {
+        fun from(quote: Quote): QuoteResponse {
+            return QuoteResponse(
+                id = quote.id ?: "",
+                content = quote.content,
+                author = quote.author
+            )
+        }
+    }
+}
+
